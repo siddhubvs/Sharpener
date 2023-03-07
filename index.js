50de92bef93dcf1d2e14f3ea6a6b71d032c0c36e
Gopal Ganji <siddhu.bvs@gmail.com>
2023-03-07 20:46:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.dir(document);
+console.log(document.forms[0]);
+var headerTitle=document.getElementById('header-title');
+//headerTitle.textContent='Hello';
+//headerTitle.innerHTML='<h1>GoodBye</h1>'
+var header=document.getElementById('main-header');
+header.style.borderTop='6px solid blue';
+header.style.borderBottom='6px solid blue';
+  
