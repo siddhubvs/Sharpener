aed115405f214e02c8dd279410c350e2613c783a
Gopal Ganji <siddhu.bvs@gmail.com>
2023-03-08 22:21:43 +0530

    dom traversals

--- index.js
+++ index.js
@@ -17,7 +17,36 @@ header.style.borderBottom='violet';
 var input=document.querySelector('input');
 input.value="Enter the name";
 
-var item=document.querySelector('li:nth-child(2)')
-item.style.color='green';
+var item2=document.querySelector('li:nth-child(2)')
+item2.style.color='green';
 var item3=document.querySelector('li:nth-child(3)')
-item.style.visibility='hidden';
+item3.style.visibility='hidden';
+
+var itemlist=document.querySelector('#items');
+console.log(itemlist.parentNode);
+itemlist.parentNode.style.backgroundColor='#f4f4f4';
+
+itemlist.children[1].style.backgroundColor='orange';
+
+itemlist.firstElementChild.textContent='Item 1 Hello'
+itemlist.lastElementChild.textContent='Item 5 Hello'
+
+//console.log(itemlist.nextElementSibling);
+itemlist.previousElementSibling.style.color='blue';
+
+var newDiv=document.createElement('div');
+newDiv.className='Hello';
+newDiv.id='Hello1';
+//newDiv.setAttribute=('title','newDiv');
+
+var text=document.createTextNode('HEllo');
+newDiv.appendChild(text);
+
+var container=document.querySelector('header .container');
+var header=document.querySelector('header h1');
+var insert=document.querySelector('div .card card-body');
+var insertbefore=document.querySelector('div .list-group-item');
+container.insertBefore(newDiv,header);
+insert.insertBefore(newDiv,insertbefore);
+newDiv.style.fontSize='40px';
+console.log(newDiv);
