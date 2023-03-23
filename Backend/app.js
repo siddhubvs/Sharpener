e4865a37b8b9550440900fd32ef007a57c916b19
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2023-03-23 22:45:14 +0530

    Added request url's and correspnding responses

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,12 @@ const http=require('http');
 
 const server=http.createServer((req,res)=>{
     console.log('siddhu')
-    res.write('siddhu');
+    if(req.url==='/home')
+    res.write('<h1>Welcome Home</h1>')
+    if(req.url==='/about')
+    res.write('<h1>Welcome to About Us Page</h1>')
+    if(req.url==='/node')
+    res.write('<h1>Welcome to NodeJS Project</h1>')
     res.end();
 })
 
