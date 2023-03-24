59357261028fb4998de0ecf0cec05a04a2468e6d
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2023-03-24 20:01:32 +0530

    created addproduct route and product route

--- Backend/express/app.js
+++ Backend/express/app.js
@@ -2,14 +2,26 @@ const express=require('express')
 
 const app=express()
 
-app.use((req,res,next)=>{
+const bodyparser=require('body-parser');
+
+app.use(bodyparser.urlencoded({extended:false}))
+
+app.use('/addProduct',(req,res,next)=>{
     console.log('In middleware');
-    next();
+    res.send("<form action='/product' method='POST'> <input type='text' name='title'> <input type='text' name='size'><button type='submit'>Send</button></form>")
 })
 
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+
+app.use('/',(req,res,next)=>{
     console.log('In another middleware');
-    res.send('<h1>Welcome to Express</h1>')
+    res.send('<h1>Welcome to express demo</h1>')
 })
 
 app.listen(4000);
+
