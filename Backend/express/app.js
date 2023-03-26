baaffd0766aada78bfb8b321d7c14375954fc312
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2023-03-26 12:33:27 +0530

    added contact path and sucess path

--- Backend/express/app.js
+++ Backend/express/app.js
@@ -4,20 +4,31 @@ const app=express()
 
 const bodyparser=require('body-parser');
 
+const path=require('path')
+
 app.use(bodyparser.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const adminroute=require('./routes/admin');
 
 const shoproute=require('./routes/shop');
 
+const contact=require('./routes/contact');
+
 app.use('/admin',adminroute);
 
 app.use('/shop',shoproute);
 
+app.use('/contact',contact);
+
+app.use('/success',(req,res,next)=>{
+    res.send('<h1>Form submitted successfully</h1>');
+})
+
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.sendFile(path.join(__dirname,'views','404.html'))
 })
 
 app.listen(4000);
-
 
