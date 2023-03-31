96d7297bcd55ac29eb6136b039fe4893a243ed98
siddhubvs <54083316+siddhubvs@users.noreply.github.com>
2023-03-31 19:02:21 +0530

    created user model with an id

--- Backend/Shopping_site/app.js
+++ Backend/Shopping_site/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,11 +8,20 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
+const sequelize=require('./util/database');
+const product=require('./models/product');
+const user=require('./models/User');
+
+user.sync();
+product.sync();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,4 +31,27 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.use((req,res,next)=>{
+    user.findByPk(1).then(user=>{
+        req.user=user;
+        next();
+    })
+    .catch(err=>console.log(err));
+})
+
+product.belongsTo(user,{constraints:true,onDelete:'CASCADE'});
+user.hasMany(product);
+
+sequelize.sync().then((result)=>{
+    return user.findByPk(1);
+})
+.then((users)=>{
+    if(!users)
+    return user.create({name:'sid',email:'sid@gmail.com'});
+    return users;
+})
+.then(user=>{
+    console.log(user);
+    app.listen(4000);
+})
+.catch(err=>console.log(err)); 
